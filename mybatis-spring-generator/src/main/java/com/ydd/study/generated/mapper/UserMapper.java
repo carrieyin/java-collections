a187e652a5e7424a47fcdfb9c52570927ee0e27f
yindd <695685009@qq.com>
2019-09-14 18:51:36 +0800

    exameple添加

--- mybatis-spring-generator/src/main/java/com/ydd/study/generated/mapper/UserMapper.java
+++ mybatis-spring-generator/src/main/java/com/ydd/study/generated/mapper/UserMapper.java
@@ -1,15 +1,28 @@
 package com.ydd.study.generated.mapper;
 
 import com.ydd.study.generated.entity.User;
+import com.ydd.study.generated.entity.UserExample;
+import java.util.List;
+import org.apache.ibatis.annotations.Param;
 
 public interface UserMapper {
+    long countByExample(UserExample example);
+
+    int deleteByExample(UserExample example);
+
     int deleteByPrimaryKey(Integer id);
 
     int insert(User record);
 
     int insertSelective(User record);
 
+    List<User> selectByExample(UserExample example);
+
     User selectByPrimaryKey(Integer id);
+
+    int updateByExampleSelective(@Param("record") User record, @Param("example") UserExample example);
+
+    int updateByExample(@Param("record") User record, @Param("example") UserExample example);
 
     int updateByPrimaryKeySelective(User record);
 
--- mybatis-spring-generator/src/test/java/generatorTest/GeneratorTest.java
+++ mybatis-spring-generator/src/test/java/generatorTest/GeneratorTest.java
@@ -1,5 +1,7 @@
 package generatorTest;
 
+import java.util.List;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.slf4j.Logger;
@@ -8,6 +10,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
+import com.ydd.study.generated.entity.User;
+import com.ydd.study.generated.entity.UserExample;
+import com.ydd.study.generated.entity.UserExample.Criteria;
 import com.ydd.study.generated.mapper.UserMapper;
 
 //https://blog.csdn.net/hellozpc/article/details/80878563#14sql_2058
@@ -18,16 +23,31 @@ import com.ydd.study.generated.mapper.UserMapper;
 */ 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration("classpath:applicationContext.xml")
-public class TestGenerator {
+public class GeneratorTest {
 	@Autowired
 	private UserMapper userMapper;
 	
-	Logger logger = LoggerFactory.getLogger(TestGenerator.class);
+	Logger logger = LoggerFactory.getLogger(GeneratorTest.class);
 	
 	@Test
 	public void testDeletByID(){
 		userMapper.deleteByPrimaryKey(7);
 	}
 	
+	@Test
+	public void testQueryWithExample(){
+		UserExample example = new UserExample();
+		
+		Criteria criteria = example.createCriteria();
+		criteria.andAgeGreaterThanOrEqualTo(17);
+		criteria.andAgeLessThan(20) ;
+		
+		criteria.andNameLike("≈Ù") ;
+        List<User> userList = userMapper.selectByExample(example);	
+        for(User u: userList){
+        	logger.info(u.toString());
+        }
+        
+	}
 	
 }
